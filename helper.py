3aa204a82dcd6abfae5b477f00b4853ccbdd0766
beubax <trishaviking@gmail.com>
2024-06-28 14:17:00 -0400

    Created Train notebook. Modified helper.py, EDA and gitignore

--- helper.py
+++ helper.py
@@ -8,7 +8,7 @@ import re
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 from catboost import CatBoostRegressor
 
@@ -96,7 +96,7 @@ def update_icd9_to_icd10(df, conversion_df, col1, col2,col3):
     
     return df
 
-def imputeBMI(df, drop_columns):
+def imputeBMI(df, drop_columns, test = pd.DataFrame(), displayRMSE = False):
 
     """
     Uses catboost to impute bmi values after dropping specific columns
@@ -117,20 +117,36 @@ def imputeBMI(df, drop_columns):
     X_val = val_data.drop(columns='bmi')
     y_val = val_data['bmi']
 
-    model = CatBoostRegressor(iterations=50, depth=6, learning_rate=0.1, loss_function='RMSE', random_state=42)
+    model = CatBoostRegressor(iterations=50, depth=6, learning_rate=0.1, loss_function='RMSE', random_state=42, verbose=0)
     model.fit(X_train, y_train)
 
     y_val_pred = model.predict(X_val)
-
-    val_rmse = mean_squared_error(y_val, y_val_pred, squared=False)
-    print(f"Validation RMSE: {val_rmse}")
+    
+    if displayRMSE:
+        val_rmse = root_mean_squared_error(y_val, y_val_pred)
+        print(f"Validation RMSE: {val_rmse}")
 
     X_target = target_data.drop(columns='bmi')
     y_pred = model.predict(X_target)
 
     missing_index = df[df['bmi'].isnull()].index
     df.loc[missing_index, 'bmi'] = np.round(y_pred,2)
-    return df
+
+    if not test.empty:
+        if test["bmi"].isnull().any():
+            drop_columns.remove("metastatic_diagnosis_period")
+            df_encoded = test.copy().drop(columns=drop_columns)
+            for col in df_encoded.select_dtypes(include=['object']):
+                df_encoded[col] = label_encoder.fit_transform(df_encoded[col])
+            target_data = df_encoded[df_encoded["bmi"].isnull()]
+            X_target = target_data.drop(columns='bmi')
+            y_pred = model.predict(X_target)
+            missing_index = test[test['bmi'].isnull()].index
+            test.loc[missing_index, 'bmi'] = np.round(y_pred,2)
+
+    if test.empty:
+        return df
+    return df, test
 
 def categorize_bmi(df):
     """
@@ -150,3 +166,66 @@ def categorize_bmi(df):
     df['bmi_category'] = df['bmi'].apply(get_bmi_category)
     
     return df
+
+def cleaning_and_null_handling(train_df, test_df, ICD_codes_df, features=[]):
+    """
+    Called in Train.ipynb to handle cleaning and null imputation before training.
+    Uses the functions above to handle cleaning and null values.
+    """
+
+    #create age groups
+    train_df = assign_age_groups(train_df)
+    test_df = assign_age_groups(test_df)
+    print("Age group assignments done.")
+
+    #Assign ICD-9 or 10 categories
+    train_df = assign_icd_codes(train_df,"breast_cancer_diagnosis_code")
+    test_df = assign_icd_codes(test_df,"breast_cancer_diagnosis_code")
+
+    #Fix any description issues
+    train_df = replace_words(train_df, "breast_cancer_diagnosis_desc")
+    test_df = replace_words(test_df, "breast_cancer_diagnosis_desc")
+
+    #Change all male codes+desc to female
+    train_df = update_codes_and_desc(train_df,"breast_cancer_diagnosis_code", "breast_cancer_diagnosis_desc")
+    test_df = update_codes_and_desc(test_df,"breast_cancer_diagnosis_code", "breast_cancer_diagnosis_desc")
+
+    #Convert all ICD-9 to 10
+    train_df = update_icd9_to_icd10(train_df,ICD_codes_df, "ICD_code","breast_cancer_diagnosis_code","breast_cancer_diagnosis_desc")
+    test_df = update_icd9_to_icd10(test_df,ICD_codes_df, "ICD_code","breast_cancer_diagnosis_code","breast_cancer_diagnosis_desc")
+    print("breast_cancer_diagnosis_code and breast_cancer_diagnosis_desc cleaning done.")
+
+    #Additional data cleaning
+    train_df.loc[(train_df["patient_state"] == "CA") & (train_df["Region"] == "West") & (train_df["Division"] == "Mountain"), "patient_state"] = "AZ"
+    train_df = train_df[~((train_df["patient_zip3"] == 630) & (train_df["patient_state"] == "IL"))]
+
+    test_df.loc[(test_df["patient_state"] == "CA") & (test_df["Region"] == "West") & (test_df["Division"] == "Mountain"), "patient_state"] = "AZ"
+    test_df = test_df[~((test_df["patient_zip3"] == 630) & (test_df["patient_state"] == "IL"))]
+
+    if features:
+        train_df = train_df[features]
+        features.remove("metastatic_diagnosis_period")
+        test_df = test_df[features]
+
+    #Fill missing bmi values using CatBoost
+    if train_df["bmi"].isnull().any():
+        remove_cols = ["age_10_to_19","race_native", "divorced", "metastatic_diagnosis_period", "patient_age","education_graduate"]
+        train_df, test_df = imputeBMI(train_df, remove_cols, test=test_df)
+
+    #Categorize bmi
+    train_df = categorize_bmi(train_df)
+    test_df = categorize_bmi(test_df)
+
+    #Drop patient_age and bmi
+    train_df = train_df.drop(columns=["bmi","patient_age"])
+    test_df = test_df.drop(columns=["bmi","patient_age"])
+    print("BMI imputation and categorization done.")
+
+    #Additional null handling
+    train_df.loc[train_df["patient_zip3"] == 361, "Average of Jun-17"] = train_df[train_df["patient_zip3"] == 360]["Average of Jun-17"].iloc[0]
+    train_df.dropna(subset=["income_household_35_to_50"], inplace=True)
+
+    test_df.loc[test_df["patient_zip3"] == 361, "Average of Jun-17"] = test_df[test_df["patient_zip3"] == 360]["Average of Jun-17"].iloc[0]
+    test_df.dropna(subset=["income_household_35_to_50"], inplace=True)
+
+    return train_df, test_df
